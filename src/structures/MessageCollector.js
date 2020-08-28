b0fb2c5426cb08e4e304c4505c103793bf1163a2
Androz2091 <androz2091@gmail.com>
2020-08-28 11:26:53 +0200

    :sparkles: Add message collectors

---
+++ src/structures/MessageCollector.js
@@ -0,0 +1,74 @@
+const { EventEmitter } = require('events')
+
+/**
+ * Collects messages in a chat
+ * @extends {EventEmitter}
+ */
+class MessageCollector extends EventEmitter {
+    /**
+     * @typedef {object} MessageCollectorOptions
+     * @property {Function} filter The filter to apply
+     * @property {number} idle How long to stop the collector after inactivity in milliseconds
+     */
+    /**
+     * @param {Chat} chat The chat in which the messages should be collected
+     * @param {MessageCollectorOptions} options The options for the collector
+     */
+    constructor (chat, { filter, idle }) {
+        super()
+        /**
+         * @type {InstaClient}
+         * The client that instantiated this
+         */
+        this.client = chat.client
+        /**
+         * @type {Chat}
+         * The chat in which the messages should be collected
+         */
+        this.chat = chat
+        /**
+         * @type {Function}
+         * The filter to apply
+         */
+        this.filter = filter
+        /**
+         * @type {number}
+         * How long to stop the collector after inactivity in milliseconds
+         */
+        this.idle = idle
+        if (idle) this._idleTimeout = setTimeout(() => this.end('idle'), this.idle)
+
+        /**
+         * @type {boolean}
+         * Whether this collector is ended
+         */
+        this.ended = false
+
+        this.handleMessage = this.handleMessage.bind(this)
+        this.client.on('messageCreate', this.handleMessage)
+    }
+
+    async handleMessage (message) {
+        const valid = await this.filter(message) && message.chatID === this.chat.id
+        if (valid) {
+            this.emit('message', message)
+
+            if (this._idleTimeout) {
+                clearTimeout(this._idleTimeout)
+                this._idleTimeout = setTimeout(() => this.end('idle'), this.idle)
+            }
+        }
+    }
+
+    /**
+     * End the collector
+     * @param {string} reason The reason the collector ended
+     */
+    async end (reason) {
+        this.removeListener('messageCreate', this.handleMessage)
+        this.ended = true
+        this.emit('end', reason)
+    }
+}
+
+module.exports = MessageCollector
