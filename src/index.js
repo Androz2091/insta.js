e3ccdef90a3f25400bba182642db714760ac4520
Androz2091 <androz2091@gmail.com>
2020-08-28 11:28:27 +0200

    :sparkles: Export all classes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+const Attachment = require('./structures/Attachment')
+const Chat = require('./structures/Chat')
 const Client = require('./structures/Client')
+const ClientUser = require('./structures/ClientUser')
+const Message = require('./structures/Message')
+const MessageCollector = require('./structures/MessageCollector')
+const User = require('./structures/User')
 
 module.exports = {
-    Client
+    Attachment,
+    Chat,
+    Client,
+    ClientUser,
+    Message,
+    MessageCollector,
+    User
 }
