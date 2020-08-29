0394fed2323d5ab38694e7880fe7a8df6ce9da8e
Snowflake <dhunganakunjan1@gmail.com>
2020-08-29 16:50:21 +0545

    esm export

---
+++ esm/index.mjs
@@ -0,0 +1,13 @@
+import Insta from '../src/index.js'
+
+export default Insta
+
+export const {
+    Attachment,
+    Chat,
+    Client,
+    ClientUser,
+    Message,
+    MessageCollector,
+    User
+} = Insta
